1508a4a0783c3c3af3b5b193f3c8ab3b79cb6166
Rashadur Rahman <60373724+Rashadur@users.noreply.github.com>
2020-04-07 21:25:07 -0600

    Update code_5_merge house data and shortest lake distance.R
    change import data path

--- code/code_5_merge house data and shortest lake distance.R
+++ code/code_5_merge house data and shortest lake distance.R
@@ -4,12 +4,12 @@
 ##Merge House data (price, area, sold year) and shortest lake distance based on "ID", "latitude and longitude (house)" (clean_HPI_HD_real_price and Final_shortest_dist)
 
 ## Import house price data (clean_HPI_HD_real_price)
-HP <-read.csv("C:/Users/rar727/OneDrive - University of Saskatchewan/rash/final data/HPI/clean_HPI_HD_real_price.csv", header = T)
+HP <-read.csv("final data/HPI/clean_HPI_HD_real_price.csv", header = T)
 HP_1 <- subset(HP, select = c("ID", "Year", "area", "Real_price"))
 
 
 ## Import shortest distance data (Final_shortest_dist)
-HSD <-read.csv("C:/Users/rar727/OneDrive - University of Saskatchewan/rash/final data/shortest dist/Final_shortest_dist.csv", header = T)
+HSD <-read.csv("final data/shortest dist/Final_shortest_dist.csv", header = T)
 
 
 ##I merged house data (HP) amd shortest lake distance (HSD) based on "ID", "Latitude", "Longitude"
@@ -20,7 +20,7 @@ HP_HSD_merge <- merge(HP_1, HSD,by=c("ID"),all=TRUE)
 
 ### Import wq data (Final_wq_data_1)
 
-Final_wq_data_1 <-read.csv("C:/Users/rar727/OneDrive - University of Saskatchewan/rash/final data/wq/Final_wq_data_1.csv", header = T)
+Final_wq_data_1 <-read.csv("final data/wq/Final_wq_data_1.csv", header = T)
 F_wq_data_1 <-subset(Final_wq_data_1, select = c("Lake_ID", "Year", "meanTP_yearly", "Secchi.depth_avg"))
 
 ##marge house data & shortest lake distance (HP_HSD_merge) and wq (F_wq_data_1) data ->> (Final_marged_wq_HD_SD)
